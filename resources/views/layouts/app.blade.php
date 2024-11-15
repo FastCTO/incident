39ed145dd5822783a2f4e5ce35d6622c91d05bdc
FastCTO <vihi@fsv.io>
2024-11-15 13:08:29 -0600

    Fixed Chrome Tab Label

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Guardian Cloud') }}</title> <!-- Updated title -->
+    <title>Guardian Cloud</title> <!-- Updated title -->
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
