0f0f5757efb90deb0e6cca16b64e8b95494564ed
FastCTO <vihi@fsv.io>
2026-05-04 23:29:27 -0500

    Add user organization relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,6 +29,11 @@ class User extends Authenticatable
         'password',
     ];
 
+    public function organization()
+    {
+        return $this->belongsTo(Organization::class);
+    }
+
     /**
      * The attributes that should be hidden for serialization.
      *
